e9e16c310d17e4bcbba1cd0fed14509b93c75516
Cajetan Grill <99743209+krokettenkoal@users.noreply.github.com>
2023-02-18 21:17:41 +0100

    Moved themes to static folder to be included in build Also updated theme loading behaviour to a simple <link> element since they're now served statically

--- src/lib/components/Theme.svelte
+++ src/lib/components/Theme.svelte
@@ -1,34 +1,12 @@
 <script context="module" lang="ts">
-    const themesFolderPath = '../../data/themes/';
-
-    export async function loadTheme(name?: string): Promise<any> {
-        if(!name)
-            return Promise.reject(`Theme overwrite removed.`);
-
-        try {
-
-            return import(
-                /* @vite-ignore */
-                `${themesFolderPath}${name}.css?raw`
-                );
-        }
-        catch(err: any){
-            return Promise.reject(err);
-        }
-    }
+    const themesFolderUrl = '/themes/';
+    export const themeUrl = (themeName: string): string => themesFolderUrl + themeName + '.css';
 </script>
 
 <script lang="ts">
     import {theme} from '../../stores';
-
-    let loading: Promise<string>;
-    $: loading = loadTheme($theme);
 </script>
 
-{#await loading}
-    <!-- LOAD THEME SILENTLY -->
-{:then module}
-    {@html `<style>${module.default}</style>`}
-    {:catch err}
-        <!-- NOTHING TO LOAD -->
-{/await}
+{#if $theme}
+    <link rel="stylesheet" href="{themeUrl($theme)}"/>
+{/if}
