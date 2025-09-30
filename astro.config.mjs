3c68ef58eb66e8ce8d8f127c07ac725fa0dd0ca7
Cajetan Grill <cajetan.grill@fhstp.ac.at>
2025-09-30 19:22:04 +0200

    build(deps): add Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
