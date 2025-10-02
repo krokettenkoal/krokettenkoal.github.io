7cfa79ebcae820250a92bd954ff6016c4eee2b2d
Cajetan Grill <cajetan.grill@fhstp.ac.at>
2025-10-02 20:52:50 +0200

    Add root layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	integrations: [svelte()],
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	site: 'https://cajetan.dev'
 });
