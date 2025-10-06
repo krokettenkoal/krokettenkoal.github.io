ae4e73c54a616b8d01139c5652005074e6895119
Cajetan Grill <cajetan.grill@fhstp.ac.at>
2025-10-06 20:47:39 +0200

    refactor: use @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import robotsTxt from 'astro-robots-txt';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), robotsTxt(), icon({
-        include: {
-            lucide: ['*'] // TODO: limit to only the icons used in the project
-        }
-    }), mdx()],
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    site: 'https://cajetan.dev'
+	integrations: [react(), icon({
+		include: {
+			lucide: ['*'] // TODO: limit to only the icons used in the project
+		}
+	}), mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	site: 'https://cajetan.dev'
 });
