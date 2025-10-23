1e2f737dba6968ef5a3b65e3e38537e09f9f2efa
Cajetan Grill <cajetan.grill@fhstp.ac.at>
2025-10-23 20:43:55 +0200

    feat: image carousel expandable

--- src/components/react/carousel-images.tsx
+++ src/components/react/carousel-images.tsx
@@ -5,8 +5,14 @@ import {
 	CarouselNext,
 	CarouselPrevious
 } from '@/components/ui/carousel';
+import {
+	Dialog,
+	DialogContent,
+	DialogTrigger
+} from '@/components/ui/dialog';
 import { CarouselIndicators } from '@/components/react/carousel-indicators';
 import type { ComponentProps } from 'react';
+import useMediaQuery from '@/lib/hooks/media-query.hook';
 
 type CarouselImage = {
 	caption: string;
@@ -18,13 +24,22 @@ type CarouselImagesProps = ComponentProps<typeof Carousel> & {
 }
 
 export function CarouselImages({ images, ...restProps }: CarouselImagesProps) {
+	const isMobile = useMediaQuery('(max-width: 48rem)'); // 768px
+
 	return (
 		<Carousel {...restProps}>
 			<CarouselContent>
 				{images.map((image, idx) => (
 					<CarouselItem key={idx}>
 						<figure className="mb-0!">
-							<img src={image.src} alt={image.caption} />
+							<Dialog>
+								<DialogTrigger disabled={isMobile}>
+									<img src={image.src} alt={image.caption} />
+								</DialogTrigger>
+								<DialogContent className="bg-transparent p-0 border-none max-w-2/3!">
+									<img src={image.src} alt={image.caption} className="w-full object-contain object-center" />
+								</DialogContent>
+							</Dialog>
 							<figcaption>{image.caption}</figcaption>
 						</figure>
 					</CarouselItem>
