a2c6993291c388e5e89b8c5390a282cd24185a40
Cajetan Grill <cajetan.grill@ustp.at>
2026-08-15 09:48:37 +0200

    feat: bio page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import react from '@astrojs/react';
@@ -10,17 +10,21 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), icon({
-		include: {
-			lucide: ['*'] // TODO: limit to only the icons used in the project
-		}
-	}), mdx(), sitemap()],
-	vite: {
-		plugins: [tailwindcss()]
-	},
-	site: 'https://cajetan.dev',
-	redirects: {
-		'/portfolio': '/projects',
-		'/portfolio/[id]': '/projects/[id]'
-	}
+    integrations: [react(), icon({
+        include: {
+            lucide: ['*'], // TODO: limit to only the icons used in the project
+            'simple-icons': ['wwise', 'chocolatey'],
+            'skill-icons': ['*'],
+            devicon: ['renpy', 'illustrator'],
+            fad: ['logo-reaper']
+        }
+    }), mdx(), sitemap()],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    site: 'https://cajetan.dev',
+    redirects: {
+        '/portfolio': '/projects',
+        '/portfolio/[id]': '/projects/[id]'
+    }
 });
--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -1,117 +1,128 @@
-import { type ComponentPropsWithoutRef, type ReactNode } from 'react';
-import { ArrowRight } from 'lucide-react';
-import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/button';
-import { TagList } from '@/components/react/tag-list';
+import {type ComponentPropsWithoutRef, type ReactNode, type ElementType} from "react"
+import {ArrowRightIcon} from "@radix-ui/react-icons"
 
-interface BentoGridProps extends ComponentPropsWithoutRef<'div'> {
-	children: ReactNode;
-	className?: string;
+import {cn} from "@/lib/utils"
+import {Button} from "@/components/ui/button"
+import {TagList} from "@/components/react/tag-list.tsx";
+
+interface BentoGridProps extends ComponentPropsWithoutRef<"div"> {
+    className?: string
 }
 
-interface BentoCardProps extends ComponentPropsWithoutRef<'div'> {
-	name: string;
-	className: string;
-	description: string;
-	href: string;
-	cta: string;
-	tags?: string[];
+interface BentoCardProps extends ComponentPropsWithoutRef<"div"> {
+    name: string
+    className: string
+    Icon?: ElementType
+    description: string
+    href: string
+    cta: string
+    IconCta?: ElementType
+    childrenWrapperProps?: ComponentPropsWithoutRef<"div">
+    tags?: string[]
 }
 
-const BentoGrid = ({ children, className, ...props }: BentoGridProps) => {
-	return (
-		<div
-			className={cn(
-				'grid w-full auto-rows-[22rem] grid-cols-3 gap-4',
-				className
-			)}
-			{...props}
-		>
-			{children}
-		</div>
-	);
-};
+const BentoGrid = ({children, className, ...props}: BentoGridProps) => {
+    return (
+        <div
+            className={cn(
+                "grid w-full auto-rows-[22rem] grid-cols-3 gap-4",
+                className
+            )}
+            {...props}
+        >
+            {children}
+        </div>
+    )
+}
 
 const BentoCard = ({
-										 name,
-										 className,
-										 description,
-										 href,
-										 cta,
-										 tags,
-										 children,
-										 ...props
-									 }: BentoCardProps) => (
-	<div
-		key={name}
-		className={cn(
-			'group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl',
-			// light styles
-			'bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]',
-			// dark styles
-			'dark:bg-background transform-gpu dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] dark:[border:1px_solid_rgba(255,255,255,.1)]',
-			className
-		)}
-		{...props}
-	>
-		<div>
-			<div
-				className="absolute inset-0 border-none [mask-image:linear-gradient(to_top,transparent_10%,#000_100%)] transition-all duration-300 ease-out group-hover:scale-105">
-				{children}
-			</div>
-		</div>
-		<div className="p-4">
-			<div
-				className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 transition-all duration-300 lg:group-hover:-translate-y-10">
-				<h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
-					{name}
-				</h3>
-				<p className="max-w-lg text-muted-foreground">{description}</p>
-				{tags?.length ? (
-					<TagList tags={tags} />
-				) : undefined}
-			</div>
+                       name,
+                       className,
+                       Icon,
+                       description,
+                       href,
+                       cta,
+                       IconCta = ArrowRightIcon,
+                       tags,
+                       children,
+                       childrenWrapperProps,
+                       ...props
+                   }: BentoCardProps) => (
+    <div
+        key={name}
+        className={cn(
+            "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
+            // light styles
+            "bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+            // dark styles
+            "dark:bg-background transform-gpu dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] dark:[border:1px_solid_rgba(255,255,255,.1)]",
+            className
+        )}
+        {...props}
+    >
+        <div>
+            <div
+                {...childrenWrapperProps}
+                className={cn("absolute inset-0 border-none mask-[linear-gradient(to_top,transparent_10%,#000_100%)] transition-all duration-300 ease-out group-hover:scale-105", childrenWrapperProps?.className)}>
+                {children}
+            </div>
+        </div>
+        <div className="p-4">
+            <div
+                className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 transition-all duration-300 lg:group-hover:-translate-y-10">
+                {Icon && (
+                    <Icon
+                        className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75"/>
+                )}
+                <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
+                    {name}
+                </h3>
+                <p className="max-w-lg text-neutral-400">{description}</p>
+                {tags?.length ? (
+                    <TagList tags={tags}/>
+                ) : undefined}
+            </div>
 
-			<div
-				className={cn(
-					'pointer-events-none flex w-full translate-y-0 transform-gpu flex-row items-center transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 lg:hidden'
-				)}
-			>
-				<Button
-					variant="link"
-					asChild
-					size="sm"
-					className="pointer-events-auto p-0 text-foreground"
-				>
-					<a href={href}>
-						{cta}
-						<ArrowRight className="ms-2 h-4 w-4 rtl:rotate-180" />
-					</a>
-				</Button>
-			</div>
-		</div>
+            <div
+                className={cn(
+                    "pointer-events-none flex w-full translate-y-0 transform-gpu flex-row items-center transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 lg:hidden"
+                )}
+            >
+                <Button
+                    variant="link"
+                    asChild
+                    size="sm"
+                    className="pointer-events-auto p-0 text-foreground"
+                >
+                    <a href={href}>
+                        {cta}
+                        <ArrowRightIcon className="ms-2 h-4 w-4 rtl:rotate-180"/>
+                    </a>
+                </Button>
+            </div>
+        </div>
 
-		<div
-			className={cn(
-				'pointer-events-none absolute bottom-0 hidden w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 lg:flex'
-			)}
-		>
-			<Button
-				variant="link"
-				asChild
-				size="sm"
-				className="pointer-events-auto p-0 text-foreground"
-			>
-				<a href={href}>
-					{cta}
-					<ArrowRight className="ms-2 h-4 w-4 rtl:rotate-180" />
-				</a>
-			</Button>
-		</div>
+        <div
+            className={cn(
+                "pointer-events-none absolute bottom-0 hidden w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 lg:flex"
+            )}
+        >
+            <Button
+                variant="link"
+                asChild
+                size="sm"
+                className="pointer-events-auto p-0 text-foreground"
+            >
+                <a href={href}>
+                    {cta}
+                    <IconCta className="ms-2 h-4 w-4 rtl:rotate-180"/>
+                </a>
+            </Button>
+        </div>
 
-		<div
-			className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />
-	</div>
-);
+        <div
+            className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/3 group-hover:dark:bg-neutral-800/10"/>
+    </div>
+)
 
-export { BentoCard, BentoGrid };
+export {BentoCard, BentoGrid}
